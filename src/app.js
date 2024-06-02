4584c169682b2f37e63c36aa0e64bb432c07033f
vitviki <vtyagi1991@gmail.com>
2024-06-02 14:54:30 +0530

    fixed front end cards

--- src/app.js
+++ src/app.js
@@ -48,6 +48,7 @@ app.get("/subscribers/:id", async (req, res) => {
 // Add a subscriber
 app.post("/subscribers/add", async (req, res) => {
   try {
+    console.log(req.body);
     const subscriber = await Subscribers.create(req.body);
     res.status(200).json(subscriber);
   } catch (error) {
